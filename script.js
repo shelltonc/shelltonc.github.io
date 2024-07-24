6469f4f36066bc268288d7189f1800b351961292
shellton caetano <shell.rzo2008@gmail.com>
2024-07-23 23:21:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ function armazenarValor(){
+ var userInput = document.getElementById("userInput").value;
+
+ console.log("O valor da variável é: " + userInput);
+
+ document.getElementById("valorInserido").innerText = "O valor inserido é: " + userInput;
+
+ }
